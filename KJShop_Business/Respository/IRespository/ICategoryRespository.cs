0b3e8b7bc7a87e33a6faed9edb64f0f58a8476f0
mrklausj <mrklausj@gmail.com>
2024-08-29 08:39:33 +0200

    Add project files.

---
+++ KJShop_Business/Respository/IRespository/ICategoryRespository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using KJShop_Models;
+
+namespace KJShop_Business.Respository.IRespository
+{
+    public interface ICategoryRespository
+    {
+        public CategoryDTO Create(CategoryDTO objDTO);
+
+        public CategoryDTO update(CategoryDTO objDTO);
+
+        public int Delete(int id);
+
+        public CategoryDTO Get(int id);
+
+        public IEnumerable<CategoryDTO> GetAll();
+    }
+}
---
+++ KJShop_Models/LBlazorModels/D_Product.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KJShop_Models.LBlazorModels
+{
+    public class D_Product
+    {
+        public int Id { get; set; }
+        public string Navn { get; set; }
+        public double Pris { get; set; }
+        public bool IsActive { get; set; }
+        public List<D_ProductProp> ProductProperties { get; set; }
+    }
+}
